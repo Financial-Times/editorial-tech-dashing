8de52f403c0d632d360c4f762c8b5df692964346
jonathan-best <jonathan.best@ft.com>
2016-03-23 10:20:42 +0000

    Add a new title to show the aggs index

--- jobs/lantern_data_hist_ingest.rb
+++ jobs/lantern_data_hist_ingest.rb
@@ -5,17 +5,33 @@ lastValue = 0
 
 SCHEDULER.every '1m', :first_in => 0, :allow_overlapping => false, :timeout => '59s' do |job|
     url = ENV['LANTERN_URL'] + '/status'
-
     response =  HTTParty.get(url, :headers => { "Accept" => "application/json" } )
     json_response = JSON.parse(response.body)
-    historical = json_response['historical']
 
+    # Status JSON
+    historical = json_response['historical']
+    realtime = json_response['realtime']
+    aggregation = json_response['aggregation']
     activePollers = json_response['activePollers']
 
-    date = DateTime.strptime(historical['latestEvent'], "%Y-%m-%d")
-    formattedDate = date.strftime("%d-%m-%Y")
+    # Realtime index date calculations
+    realDate = DateTime.iso8601(realtime['docDate'])
+    realFormattedDate = realDate.strftime("%d-%m-%Y")
+    realHour = realDate.strftime("%H:%M")
+    realStatus = realtime['status'];
 
-    send_event('LANTERN_DATA_HIST_INGEST', { metric: formattedDate, type: 'historical' })
+    # Histroical index date calculations
+    histDate = DateTime.strptime(historical['latestEvent'], "%Y-%m-%d")
+    histFormattedDate = histDate.strftime("%d-%m-%Y")
+
+    # Aggregation index date calculations
+    aggDate = DateTime.strptime(aggregation['latestEvent'], "%Y-%m-%d")
+    aggFormattedDate = aggDate.strftime("%d-%m-%Y")
+
+    # Send event to the dashboard
+    send_event('LANTERN_DATA_HIST_INGEST', { metric: histFormattedDate, type: 'historical' })
+    send_event('LANTERN_DATA_REAL_INGEST', { status: realStatus, metric: realFormattedDate, hour: realHour, type: 'realtime' })
+    send_event('LANTERN_DATA_AGG_INGEST', { metric: aggFormattedDate, type: 'aggreagtion' })
     send_event('LANTERN_DATA_POLLERS', { current: activePollers, last: lastValue })
 
     lastValue = activePollers
--- lib/circleci.rb
+++ lib/circleci.rb
@@ -8,17 +8,17 @@ def translate_status_to_class(status)
   statuses[status] || 'pending'
 end
 
-def update_builds(project, auth_token) 
+def update_builds(project, auth_token)
   api_url = 'https://circleci.com/api/v1/project/%s/%s?circle-token=%s'
   api_url = api_url % [project[:user], project[:repo], auth_token]
 
   api_response =  HTTParty.get(api_url, :headers => { "Accept" => "application/json" } )
   api_json = JSON.parse(api_response.body)
   return {} if api_json.empty?
-    
+
   latest_builds = api_json.select{ |build| build['status'] != 'queued' }.first(12)
   data = Array.new;
-    
+
   latest_builds.each do |build|
       data.push({
           repo: "#{project[:repo]}",
@@ -27,6 +27,6 @@ def update_builds(project, auth_token)
           widget_class: "#{translate_status_to_class(build['status'])}"
       })
   end
-        
+
   return data
 end
