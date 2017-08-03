0a20bd3d15da3d301439def85bb94afd838da476
ola.okejimi <ola.okejimi@ft.com>
2017-08-03 14:14:07 +0100

    Point martjob endpoint to new martjob monitor

--- jobs/mart_job.rb
+++ jobs/mart_job.rb
@@ -2,24 +2,24 @@ require 'httparty'
 require 'json'
 
 SCHEDULER.every '1m', :first_in => 0, :allow_overlapping => false, :timeout => '59s' do |job|
-    
+
    data = Array.new;
-   urls = ['http://52.50.35.34/process-new-pages',
-           'http://52.50.35.34/articlemetadata-cdc',
-           'http://52.50.35.34/article-insights',
-           'http://52.50.35.34/unload-trigger']
-    
-   urls.each do |url|   
-      name = (url.split('/')[-1]) 
+   urls = ['http://52.51.21.228/process-new-pages',
+           'http://52.51.21.228/articlemetadata-cdc',
+           'http://52.51.21.228/article-insights',
+           'http://52.51.21.228/unload-trigger']
+
+   urls.each do |url|
+      name = (url.split('/')[-1])
       response =  HTTParty.get(url, :headers => { "Accept" => "application/json" } )
       json_response = JSON.parse(response.body)
-       
+
       if json_response["status"] == "UP"
            data.push({
               name: name,
               status: json_response['status']
            })
-       else 
+       else
            data.push({
               name: name,
               status: json_response['status'],
@@ -27,7 +27,7 @@ SCHEDULER.every '1m', :first_in => 0, :allow_overlapping => false, :timeout => '
               lateness: json_response['lateness']
            })
        end
-   end   
-    
-   send_event('MART_JOB', { items: data }) 
+   end
+
+   send_event('MART_JOB', { items: data })
 end
